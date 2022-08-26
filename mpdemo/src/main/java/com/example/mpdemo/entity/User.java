b14fb88505a257c0603a979004c342f7672299ae
mpdemo <1047626638@qq.com>
2022-08-26 17:21:56 +0800

    20220/08/26 17:21 mp修改操作

--- mpdemo/src/main/java/com/example/mpdemo/entity/User.java
+++ mpdemo/src/main/java/com/example/mpdemo/entity/User.java
@@ -1,12 +1,24 @@
 package com.example.mpdemo.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
+    @TableId(type = IdType.AUTO) //auto:递增
     private Long id;
     private String name;
     private Integer age;
     private String email;
+    //自动填充
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 
 }
--- mpdemo/src/test/java/com/example/mpdemo/MpdemoApplicationTests.java
+++ mpdemo/src/test/java/com/example/mpdemo/MpdemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.mpdemo;
 
 import com.example.mpdemo.entity.User;
 import com.example.mpdemo.mapper.UserMapper;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +11,26 @@ import java.util.List;
 
 @SpringBootTest
 public class MpdemoApplicationTests {
+    @Autowired
+    public UserMapper userMapper;
 
     @Test
     public void contextLoads() {
+//        List<User> list = userMapper.selectList(null);
+//        System.out.println(list);
+        User user = new User();
+        user.setName("lucy");
+        user.setAge(35);
+        user.setEmail("1057584587@qq.com");
+        userMapper.insert(user);
+    }
+    @Test
+    public void update(){
+        User user = new User();
+        user.setId(1562976244906180613L);
+        user.setAge(60);
+        int i = userMapper.updateById(user);
+        System.out.println(i);
 
     }
 
